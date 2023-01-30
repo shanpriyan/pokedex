b6e9e58a90a8cba195ca815b1ad4bde06ebfede4
Shanmughapriyan S <priyanshan03@gmail.com>
2023-01-30 09:31:40 +0530

    chore: update intersectionobserver (#56)

--- src/hooks/useIntersectionObserver.jsx
+++ src/hooks/useIntersectionObserver.jsx
@@ -2,22 +2,31 @@ import { useEffect, useRef } from "react";
 
 export const useIntersectionObserver = (callback, deps = []) => {
   const ref = useRef(null);
+  const intersectionObserver = useRef(null);
 
   useEffect(() => {
     if (!window.IntersectionObserver || !ref?.current) return;
     const node = ref.current;
 
-    function handleEntries([entry]) {
-      if (entry.isIntersecting) {
-        callback();
+    function unobserve() {
+      if (node) {
+        intersectionObserver.current?.unobserve(node);
       }
     }
 
-    const options = { threshold: 0.1 };
-    const observer = new IntersectionObserver(handleEntries, options);
-    observer.observe(node);
+    function handleEntries([entry]) {
+      if (entry.isIntersecting) {
+        callback();
+        unobserve();
+      }
+    }
 
-    return () => observer.unobserve(node);
+    intersectionObserver.current = new IntersectionObserver(handleEntries, {
+      threshold: 0.1,
+    });
+    intersectionObserver.current.observe(node);
+
+    return unobserve;
   }, deps);
 
   return ref;
